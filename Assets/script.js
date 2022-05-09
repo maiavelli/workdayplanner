53a44899b7b18cf572fca631afd5b1113c75999e
Maia Davis <maia@Maias-Air.fios-router.home>
2022-05-09 17:11:17 -0400

    added function to change timeblock colors

--- Assets/script.js
+++ Assets/script.js
@@ -12,18 +12,36 @@ function rightNow(){
 var saveButton = $(".saveBtn");
 saveButton.on("click", function() {
     //variable for user input
-    var input = $(this).siblings(".hour").val();
+    var input = $(this).siblings(".timeblock").val();
     //variable for selected timeblock
-    var selectedHour = $(this).siblings(".hour").attr("id")
+    var selectedHour = $(this).siblings(".timeblock").attr("id");
     //save input and hour in local storage
     localStorage.setItem(selectedHour, input);
 })
+
 
 //get current hour from moment.js
 var currentHour = moment().hours();
 console.log(currentHour);
 //compare current time to timeblock
-$(".hour").each(function () {
+$(".timeblock").each(function () {
     var calendarHour = parseInt($(this).attr("id"));
-})
-    
+    console.log(calendarHour);
+
+
+//if statements to determine which color to display on each timeblock
+if (calendarHour === currentHour) {
+    $(this).addClass("present");
+    $(this).removeClass("future");
+    $(this).removeClass("past");
+} else if (calendarHour > currentHour) {
+    $(this).addClass("future");
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+} else {
+    $(this).addClass("past");
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+}
+});
+
