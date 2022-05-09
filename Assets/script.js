a000cb242133221d9a59e84437a2a11a8bfe600a
Maia Davis <maia@Maias-Air.fios-router.home>
2022-05-09 17:26:49 -0400

    added function for pulling data from local storage

--- Assets/script.js
+++ Assets/script.js
@@ -45,3 +45,14 @@ if (calendarHour === currentHour) {
 }
 });
 
+
+// pull data from local storage on refresh and back into timeblocks
+$("#9").val(localStorage.getItem("9"));
+$("#10").val(localStorage.getItem("10"));
+$("#11").val(localStorage.getItem("11"));
+$("#12").val(localStorage.getItem("12"));
+$("#13").val(localStorage.getItem("13"));
+$("#14").val(localStorage.getItem("14"));
+$("#15").val(localStorage.getItem("15"));
+$("#16").val(localStorage.getItem("16"));
+$("#17").val(localStorage.getItem("17"));
