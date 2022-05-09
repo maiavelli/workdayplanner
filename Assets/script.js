5e59eb1ce3e26ade82f83ae07cee9dc4ffb1e63f
Maia Davis <maia@Maias-Air.fios-router.home>
2022-05-09 16:48:54 -0400

    began functions and variables for timeblocks in javascript, function to compare calendar time to current time

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,4 @@
 //display current date and time in jumbotron
-
 function rightNow(){
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
     }
@@ -7,4 +6,24 @@ function rightNow(){
     setInterval(rightNow, 1000);
     
     console.log(moment().format("MM ddd, YYYY hh:mm:ss a"));
+
+
+// local storage function
+var saveButton = $(".saveBtn");
+saveButton.on("click", function() {
+    //variable for user input
+    var input = $(this).siblings(".hour").val();
+    //variable for selected timeblock
+    var selectedHour = $(this).siblings(".hour").attr("id")
+    //save input and hour in local storage
+    localStorage.setItem(selectedHour, input);
+})
+
+//get current hour from moment.js
+var currentHour = moment().hours();
+console.log(currentHour);
+//compare current time to timeblock
+$(".hour").each(function () {
+    var calendarHour = parseInt($(this).attr("id"));
+})
     
